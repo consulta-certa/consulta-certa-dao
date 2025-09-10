bf3491be9f2e97a3f6713318bfb6443649dd433c
Gustavo-Bosak <gustavobosaksantos@gmail.com>
2025-09-10 10:40:50 -0300

    Construção da classe ConnectionFactory e tratamento de suas possíveis exceções

---
+++ src/br/com/fiap/connection/ConnectionFactory.java
@@ -0,0 +1,26 @@
+package br.com.fiap.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    public Connection getConnection () {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+//          Futuramente vou atualizar com variáveis de ambiente, por questão de boas práticas de segurança
+            return DriverManager.getConnection(
+                "jdbc:oracle:thin:@oracle.fiap.com.br:1512:ORCL",
+                "RM566315",
+                "050304"
+            );
+        } catch (ClassNotFoundException e) {
+            System.err.println("Driver JDBC não encontrado: " + e.getMessage());
+            throw new RuntimeException("Erro ao carregar o driver JDBC", e);
+        } catch (SQLException e) {
+            System.err.println("Conexão com banco não estabelecida: " + e.getMessage());
+            throw new RuntimeException("Erro de conexão com o banco", e);
+        }
+    }
+}
